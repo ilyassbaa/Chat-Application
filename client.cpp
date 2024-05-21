2d74ca91700f69d6665bf7a78710746e069e3f57
ayoub amghar <154932852+AYB333@users.noreply.github.com>
2024-05-21 13:17:31 +0100

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -5,7 +5,6 @@
 #include <ws2tcpip.h>
 
 #pragma comment(lib, "Ws2_32.lib")
-
 #define PORT 12345
 
 void receive_messages(SOCKET client_socket) {
@@ -14,11 +13,11 @@ void receive_messages(SOCKET client_socket) {
         memset(buffer, 0, 1024);
         int bytes_received = recv(client_socket, buffer, 1024, 0);
         if (bytes_received <= 0) {
-            std::cerr << "Connexion au serveur perdue." << std::endl;
+            std::cerr << "Connexion au serveur perdue !" << std::endl;
             closesocket(client_socket);
             exit(1);
         } else {
-            std::cout << "Message recu: " << buffer << std::endl;
+            std::cout << "Message recu : " << buffer << std::endl;
         }
     }
 }
@@ -26,13 +25,13 @@ void receive_messages(SOCKET client_socket) {
 int main() {
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "Echec de l'initialisation de Winsock." << std::endl;
+        std::cerr << "Echec de l'initialisation de Winsock !" << std::endl;
         return 1;
     }
 
     SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket == INVALID_SOCKET) {
-        std::cerr << "Impossible de creer le socket." << std::endl;
+        std::cerr << "Impossible de creer le socket " << std::endl;
         WSACleanup();
         return 1;
     }
@@ -43,13 +42,13 @@ int main() {
     inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);  // Replace with server IP address if necessary
 
     if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        std::cerr << "Connexion au serveur echouee." << std::endl;
+        std::cerr << "Connexion au serveur echouee !" << std::endl;
         closesocket(client_socket);
         WSACleanup();
         return 1;
     }
 
-    std::cout << "Connecte au serveur." << std::endl;
+    std::cout << "Connecte au serveur " << std::endl;
 
     std::thread(receive_messages, client_socket).detach();
 
@@ -57,7 +56,7 @@ int main() {
     while (true) {
         std::cin.getline(buffer, 1024);
         if (send(client_socket, buffer, strlen(buffer), 0) == SOCKET_ERROR) {
-            std::cerr << "Envoi du message echoue." << std::endl;
+            std::cerr << "l'Envoi du message echoue !" << std::endl;
             break;
         }
     }
