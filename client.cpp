e5bc84baa317cfe10a256596f7e1d4b5169e6398
ilyass bahri <94056566+ilyassbaa@users.noreply.github.com>
2024-05-28 21:31:40 +0100

    Update the code

--- client.cpp
+++ client.cpp
@@ -5,71 +5,64 @@
 #include <ws2tcpip.h>
 
 #pragma comment(lib, "Ws2_32.lib")
+
 #define PORT 12345
 
-// Fonction pour recevoir les messages du serveur
-void receive_messages(SOCKET client_socket) {
+void recevoir_messages(SOCKET client_socket) {
     char buffer[1024];
     while (true) {
-        memset(buffer, 0, 1024); // Remplit le buffer de zéros
-        int bytes_received = recv(client_socket, buffer, 1024, 0); // Reçoit les données du serveur
-        if (bytes_received <= 0) { // Vérifie si la connexion est perdue
-            std::cerr << "Connexion au serveur perdue !" << std::endl;
-            closesocket(client_socket); // Ferme le socket
-            exit(1); // Termine le programme
+        memset(buffer, 0, 1024);
+        int bytes_received = recv(client_socket, buffer, 1024, 0);
+        if (bytes_received <= 0) {
+            std::cerr << "Connexion au serveur perdue." << std::endl;
+            closesocket(client_socket);
+            exit(1);
         } else {
-            std::cout << "Message recu : " << buffer << std::endl; // Affiche le message reçu
+            std::cout << "Message reçu: " << buffer << std::endl;
         }
     }
 }
 
 int main() {
     WSADATA wsaData;
-    // Initialise Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "Echec de l'initialisation de Winsock !" << std::endl;
+        std::cerr << "Échec de l'initialisation de Winsock." << std::endl;
         return 1;
     }
 
-    // Crée un socket client
     SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket == INVALID_SOCKET) {
-        std::cerr << "Impossible de creer le socket " << std::endl;
+        std::cerr << "Impossible de créer le socket." << std::endl;
         WSACleanup();
         return 1;
     }
 
-    sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET; // Utilise IPv4
-    server_addr.sin_port = htons(PORT); // Définit le port
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr); // Convertit l'adresse IP
+    sockaddr_in adresse_serveur;
+    adresse_serveur.sin_family = AF_INET;
+    adresse_serveur.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &adresse_serveur.sin_addr);  // Remplacez par l'adresse IP du serveur si nécessaire
 
-    // Tente de se connecter au serveur
-    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        std::cerr << "Connexion au serveur echouee !" << std::endl;
+    if (connect(client_socket, (struct sockaddr*)&adresse_serveur, sizeof(adresse_serveur)) == SOCKET_ERROR) {
+        std::cerr << "Connexion au serveur échouée." << std::endl;
         closesocket(client_socket);
         WSACleanup();
         return 1;
     }
 
-    std::cout << "Connecte au serveur " << std::endl;
+    std::cout << "Connecté au serveur." << std::endl;
 
-    // Lance un thread pour recevoir les messages du serveur
-    std::thread(receive_messages, client_socket).detach();
+    std::thread(recevoir_messages, client_socket).detach();
 
     char buffer[1024];
     while (true) {
-        std::cin.getline(buffer, 1024); // Lit une ligne de l'entrée standard
-        // Envoie le message au serveur
+        std::cin.getline(buffer, 1024);
         if (send(client_socket, buffer, strlen(buffer), 0) == SOCKET_ERROR) {
-            std::cerr << "l'Envoi du message echoue !" << std::endl;
-            break; // Sort de la boucle en cas d'erreur d'envoi
+            std::cerr << "Envoi du message échoué." << std::endl;
+            break;
         }
     }
 
-    // Ferme le socket et nettoie Winsock
     closesocket(client_socket);
     WSACleanup();
-    
     return 0;
 }
